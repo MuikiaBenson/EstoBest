9564201a6b910ce1c713dbddcdc44e94ec9eeb66
Muikia Benson <benmuigai20@gmail.com>
2024-07-09 19:15:03 +0300

    Deploy esto-best-frontend to GitHub Pages

--- property-service/utils/auth.js
+++
@@ -1,9 +0,0 @@
-// utils/auth.js
-const jwt = require('jsonwebtoken');
-
-const generateToken = (payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-  return token;
-};
-
-module.exports = generateToken;
